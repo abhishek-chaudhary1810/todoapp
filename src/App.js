249b62c69793cd2c0fb8029c8ab2b64cb583d6bd
Abhishek Chaudhary <abhishekchoudharuy1810@gmail.com>
2023-04-06 14:25:41 +0530

    Feat: form data can be added to tastlist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Todo from './Components/Todo';
 import Form from './Components/Form';
 import Filterbutton from './Components/Filterbutton';
+import React,{ useState } from 'react';
+
 
 function App(props) {
-  const tasklist = props.tasks.map((task)=> (<Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />)) ;//conversion of array to object to map 
+  const [tasks,setTasks] = useState(props.tasks);  
+  const tasklist = tasks.map((task)=> (<Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />)) ;//conversion of array to object to map 
+  
+  function addTask(name){
+    const newTask={id:"id",name, completed:false}; //object of name as name is string and cant be setted to array of object
+    setTasks([...tasks,newTask]);
+  }
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-     <Form />
+     <Form addTask={addTask}/> 
       <h2 id="list-heading">3 tasks remaining</h2>
       <Filterbutton />
       <Filterbutton />
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,23 +1,38 @@
-import React from "react";
-export default function Form(){
-    return(
-        <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-
-    );
+import React, { useState } from "react";
+//Below Function is add to handle events while adding
+export default function Form(props) {
+  const [name, setName] = useState(""); // Initializing and defining Hooks
+ //Reading and Logging Changing User Inputs
+  function handleChange(e) {
+    const { value } = e.target;
+    setName(value);  
+  }
+//Submitting data and resetting setName or state
+  function handleSubmit(e){
+    e.preventDefault();
+  
+    props.addTask(name);
+    setName(""); //Setting setname again to empty string
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={(e) => handleChange(e)}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
 }
